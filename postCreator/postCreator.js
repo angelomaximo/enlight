7a3d37d1969b0d375ffca99275529ee41828bf3e
Angelo <angelomaximodd@outlook.com>
2022-08-07 18:42:20 -0300

    Save through button working. Just need reload.

--- postCreator/postCreator.js
+++ postCreator/postCreator.js
@@ -4,14 +4,18 @@ function getRandomNum(min, max) {
 }
 
 
-const tagInp = document.getElementById("tagInp");
 const headlineInp = document.getElementById("headlineInp");
 const subHeadlineInp = document.getElementById("subHeadlineInp");
+const domainInp = document.getElementById("domainInp")
+const tagInp = document.getElementById("tagInp");
 const btn = document.getElementById("btn");
 const headlineTxt = document.getElementById("headlineTxt");
 const subHeadlineTxt = document.getElementById("subHeadlineTxt");
+const domainTxt = document.getElementById("domainTxt");
 const generate = document.getElementById("generate");
 const imgContainer = document.getElementById("imgContainer");
+const save = document.getElementById("save");
+
 
 
 let n = getRandomNum(1, 5);
@@ -21,10 +25,12 @@ btn.addEventListener('click', (e) => {
 
     const headline = headlineInp.value;
     const subHeadline = subHeadlineInp.value;
+    const domain = domainInp.value;
     const tag = tagInp.value;
 
-    headlineTxt.innerHTML = `${headline}`
-    subHeadlineTxt.innerHTML = `${subHeadline}`
+    headlineTxt.innerHTML = `${headline}`;
+    subHeadlineTxt.innerHTML = `${subHeadline}`;
+    domainTxt.innerHTML = `${domain}`;
 
     if (tag == 'finance') {
         imgContainer.innerHTML = `<img class="img" src="./img/finance/img${n}.jpg">`
@@ -35,13 +41,14 @@ btn.addEventListener('click', (e) => {
     } else if (tag == 'technology') {
         imgContainer.innerHTML = `<img class="img" src="../img/(finance)stock-blur.jpg">`
     }
-
 })
 
 function doCapture() {
     window.scrollTo(0, 0);
 
     html2canvas(document.getElementById('generate')).then(function(canvas) {
-        console.log(canvas.toDataURL("image/jpeg", 0.9))
+        console.log(canvas.toDataURL("image/jpeg", 0.9));
+        const thePrint = canvas.toDataURL("image/jpeg", 0.9)
+        save.innerHTML = `<br><a href="${thePrint}" target="_blank" class="btn btn-block btn-warning">SAVE</a>`;
     });
 }
